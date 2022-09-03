a081fd024be53347c6b8a91e45834f0aae5dcb01
Mudassir.pasha <mudassirpasha6666@gmail.com>
2022-09-03 15:53:35 +0000

    Modified helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,10 +1,5 @@
-class helloworld 
-{ 
-    // Your program begins with a call to main(). 
-    // Prints "Hello, World" to the terminal window. 
-    public static void main(String args[]) 
-    { 
-        System.out.println("Hello World and welcome to java"); 
-    } 
+class HelloWorld{ 
+public static void main(String a[]){
+System.out.println("Hello World");
 }
-
+}
